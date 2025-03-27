352defb075950a0d0554ed413b76fed2b745f3d1
张之阳 <51194195+luoliwoshang@users.noreply.github.com>
2025-03-27 17:21:45 +0800

    Release-as: libxslt/v1.0.0 (#9)

---
+++ libxslt/exsltconfig.go
@@ -0,0 +1,8 @@
+package libxslt
+
+import _ "unsafe"
+
+const LIBEXSLT_DOTTED_VERSION = "1.1.34"
+const LIBEXSLT_VERSION = 820
+const LIBEXSLT_VERSION_STRING = "820"
+const LIBEXSLT_VERSION_EXTRA = ""
