931d41794ea4bf1c792f4379a6efb17375687573
luoliwoshang <2643523683@qq.com>
2025-04-01 15:42:36 +0800

    libxml2:trim LIBXML_

--- libxml2/xmlversion.go
+++ libxml2/xmlversion.go
@@ -2,8 +2,8 @@ package libxml2
 
 import _ "unsafe"
 
-const LIBXML_DOTTED_VERSION = "2.13.6"
-const LIBXML_VERSION = 21306
-const LIBXML_VERSION_STRING = "21306"
-const LIBXML_VERSION_EXTRA = ""
-const LIBXML_MODULE_EXTENSION = ".so"
+const DOTTED_VERSION = "2.13.6"
+const VERSION = 21306
+const VERSION_STRING = "21306"
+const VERSION_EXTRA = ""
+const MODULE_EXTENSION = ".so"
